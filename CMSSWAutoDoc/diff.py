878fe8ee059ea13013e047e7eeb7a98d4df709aa
Mehmet <mehmet3@gmail.com>
2014-10-10 17:23:14 +0200

    print environ error

--- CMSSWAutoDoc/diff.py
+++ CMSSWAutoDoc/diff.py
@@ -1,1 +1,9 @@
+import os, sys
 import tools.url, tools.fileOps
+
+print 'ho ho o'
+if (not 'DATA' in os.environ) or (not 'TMP' in os.environ):
+    print >> sys.stderr, 'ERROR: TMP and DATA variables could not be found. (do you forget the source init.sh?)'
+    sys.exit(1)
+data = os.environ['DATA']
+tmp  = os.environ['TMP']
