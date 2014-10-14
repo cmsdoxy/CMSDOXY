0186ef2179be8ac158afefd3d4b733155fd0cb36
Mehmet <mehmet3@gmail.com>
2014-10-14 18:17:30 +0200

    initial commit

---
+++ CMSSWAutoDoc/doc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+WORK_DIR=$(pwd)
+BASE=$(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+cd $BASE
+source init.sh
+
+echo $TMP
+
+OUTPUT=$(python find.py $IOFILE)
+REL=$(echo $OUTPUT | cut -f 1 -d ' ')
+ARCH=$(echo $OUTPUT | cut -f 2 -d ' ')
+
+echo "Documenting $REL ($ARCH)..." #>> $LOG
+
+# set the architecture
+export SCRAM_ARCH=$ARCH
+cd $TMP
+
+# create the CMSSW base
+#cmsrel $REL # huh?
+scramv1 project CMSSW $REL
+cd $REL
+
+# set the cms env
+#cmsenv # huh?
+eval `scramv1 runtime -sh`
+
+# clone CMSSW repo
+git clone https://github.com/cms-sw/cmssw.git src
+cd src/
+git checkout $REL
+cd ..
+
+# run the documentation
+
+# clean up the base
+cd $TMP/$REL
+#rm -rf 
+
+cd $WORK_DIR
