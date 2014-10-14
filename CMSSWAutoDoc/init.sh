e885734d6ec7b0e5e575a5f0158af17eae4d5775
Mehmet <mehmet3@gmail.com>
2014-10-14 18:15:33 +0200

    separate tmp directories

--- CMSSWAutoDoc/init.sh
+++ CMSSWAutoDoc/init.sh
@@ -7,10 +7,15 @@ cd $BASE
 
 export NAME=CMSSWAutoDoc
 export CONFIG=$DATA/$NAME/conf.json
+export TMP=$TMP/$NAME
 export IOFILE=$TMP/cmsswRels2Doc.json
 export LOG_PATH=$LOGS/$NAME
 export LOG=$LOG_PATH/$(date +%Y%m%d_%H%M).log
 
 if [ ! -d "$LOG_PATH" ]; then
     mkdir $LOG_PATH
+fi
+
+if [ ! -d "$TMP" ]; then
+    mkdir $TMP
 fi
