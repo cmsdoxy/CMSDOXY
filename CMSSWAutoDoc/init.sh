be2bf5526e9f490a0e13855ab807b8b707ce8fac
cmsdoxy <cmsdoxy@cern.ch>
2014-10-23 12:09:08 +0200

    update log file naming

--- CMSSWAutoDoc/init.sh
+++ CMSSWAutoDoc/init.sh
@@ -10,7 +10,7 @@ export CONFIG=$DATA/$NAME/conf.json
 export TMP=$TMP/$NAME
 export IOFILE=$TMP/cmsswRels2Doc.json
 export LOG_PATH=$LOGS/$NAME
-export LOG=$LOG_PATH/$(date +%Y%m%d_%H%M).log
+export LOG_DATE=$(date +%Y%m%d_%H%M)
 
 if [ ! -d "$LOG_PATH" ]; then
     mkdir $LOG_PATH
@@ -21,6 +21,6 @@ if [ ! -d "$TMP" ]; then
 fi
 
 # Redirect stdout ( > ) into a named pipe ( >() ) running "tee"
-exec > >(tee -a $LOG)
+exec > >(tee -a "${LOG_PATH}/${LOG_DATE}.log")
 # capture stderr
 exec 2>&1
